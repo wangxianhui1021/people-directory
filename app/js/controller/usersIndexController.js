b5a523a4851c421cf93014dcc7c15853f01989d4
xianhui Wang <wangxianhui1021@gmail.com>
2015-07-26 18:01:42 -0500

    create pagination functions and update search

--- app/js/controller/usersIndexController.js
+++ app/js/controller/usersIndexController.js
@@ -1,4 +1,19 @@
-angular.module('myApp').controller('UsersIndexController', function(User, $scope, AuthenticationService){
+angular.module('myApp').controller('UsersIndexController', function(User, $scope, $filter) {
+
+
+    $scope.currentPage = 0;
+    $scope.pageSize = 20;
+
+
+    $scope.numberOfPages = function () {
+        var myFilteredData = $filter('filter')($scope.users, $scope.search_input);
+        return Math.ceil(myFilteredData.length/ $scope.pageSize);
+    }
+
+
     $scope.users = User.query();
+
+
+
 
 });
---
+++ app/js/filter/startFrom.js
@@ -0,0 +1,8 @@
+angular.module('myApp').filter('startFrom', function() {
+        return function(input, start) {
+            start = +start;
+            return input.slice(start);
+        };
+    }
+
+)
