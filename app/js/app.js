58fa08edf05c3c61cdb35ddee7e71b7677f7aac4
xianhui Wang <wangxianhui1021@gmail.com>
2015-07-26 21:20:54 -0500

    add styles to the landing page.

--- app/js/app.js
+++ app/js/app.js
@@ -7,21 +7,3 @@ angular.module('myApp', [
     'Authentication',
     'ngCookies'
 ])
-    .run(['$rootScope', '$location', '$cookieStore', '$http',
-  function ($rootScope, $location, $cookieStore, $http) {
-
-    // keep user logged in after page refresh
-
-    $rootScope.globals = $cookieStore.get('globals') || {};
-    if ($rootScope.globals.currentUser) {
-      $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-    }
-
-    $rootScope.$on('$locationChangeStart', function (event, next, current) {
-      // redirect to login page if not logged in
-      if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-
-        $location.path('/users');
-      }
-    });
-  }]);
